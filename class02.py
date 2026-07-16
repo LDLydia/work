ef37ca9868d30839ab381ecd9a4fe512498de912
LDLydia <ld.lydia555@gmail.com>
2026-07-16 10:58:30 +0700

    add file

---
+++ class02.py
@@ -0,0 +1,29 @@
+#print("Hello world")
+
+# price = float(input("กรอกราคาของคุณ: "))
+# quantity = int(input("กรอกจำนวนสินค้าของคุณ: "))
+
+# total = price * quantity
+# print("ราคารวมทั้งหมด: ", total, "บาท")
+
+# text1 = "hello"
+# text2 = "Hello"
+# print(text1 == text2)
+
+# a = "10"
+# b = "5"
+# print(a + b)
+
+# name = input ("Enter Your name: ")
+# age = 18
+
+# print(f"คุณชื่อว่า {name} อายุ {age} ปี")
+
+# x = 5
+
+# if x == 6:
+#     print("ใช่")
+# elif x == 5:
+#     print("มันคือห้า")
+# else:
+#     print("ไม่ใช่หก")
